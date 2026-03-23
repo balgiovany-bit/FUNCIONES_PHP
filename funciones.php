fc889ba06ec60e3ef30d13e0833b40c1271cb2d5
Giovany Bal Roquel <balgiovany@gmail.com>
2026-03-23 13:29:37 -0600

    var que nos ayuda a depurar

--- funciones.php
+++ funciones.php
@@ -3,4 +3,8 @@ isset($variable); //Verifica si una variable está definida y no es null.
 
 empty($variable); //Comprueba si una variable está vacía.
 
-gettype($variable); //Devuelve el tipo de dato de la variable
+gettype($variable); //Devuelve que tipo de dato de la variable
+
+is_array($variable); //Verifica si el contenido es un arreglo.
+
+var_dump($variable); //Muestra información detallada de una variable se sutiliza para la depuracion de datos
