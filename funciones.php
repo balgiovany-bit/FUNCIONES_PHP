bf27ca4c0e6d3c5bcf576ac8d39ee298454f621d
Giovany Bal Roquel <balgiovany@gmail.com>
2026-03-23 13:26:11 -0600

    tipo de variable

--- funciones.php
+++ funciones.php
@@ -2,3 +2,5 @@
 isset($variable); //Verifica si una variable está definida y no es null.
 
 empty($variable); //Comprueba si una variable está vacía.
+
+gettype($variable); //Devuelve el tipo de dato de la variable
