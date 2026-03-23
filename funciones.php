0b9cc70ee6bfd3e5c40c98b2f01da343daaf1a84
Giovany Bal Roquel <balgiovany@gmail.com>
2026-03-23 13:31:02 -0600

    funcion que devuelve la longitud de una cadena

--- funciones.php
+++ funciones.php
@@ -1,4 +1,4 @@
-<?
+<?php
 isset($variable); //Verifica si una variable está definida y no es null.
 
 empty($variable); //Comprueba si una variable está vacía.
@@ -8,3 +8,6 @@ gettype($variable); //Devuelve que tipo de dato de la variable
 is_array($variable); //Verifica si el contenido es un arreglo.
 
 var_dump($variable); //Muestra información detallada de una variable se sutiliza para la depuracion de datos
+
+strlen($cadena); //Devuelve la longitud de una cadena
+
